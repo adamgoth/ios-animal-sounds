d8a741edc40a0db877018225e5441e74c706c2a8
Adam Goth <adamgoth@gmail.com>
2016-08-02 06:53:34 -0600

    Initial Commit

---
+++ animal-sounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animal-sounds
+//
+//  Created by Adam Goth on 8/2/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
