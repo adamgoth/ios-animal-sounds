908b623c4f9e7ca9d973ee5d7e2aa9c8d791d563
Adam Goth <adamgoth@gmail.com>
2016-08-03 20:06:02 -0600

    Adds custom view for cell for rounded corners, adds custom font to navigation bar

---
+++ animal-sounds/AnimalContainerView.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalContainerView.swift
+//  animal-sounds
+//
+//  Created by Adam Goth on 8/3/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class AnimalContainerView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+
+}
